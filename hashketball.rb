bda58918ce545cdea8b7281a0ded87b927ceaa76
Jessica Triana <jessica.triana.tx@gmail.com>
2020-01-17 20:43:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -209,7 +209,7 @@ end
 def most_points_scored
 #return player with most points
 game_hash.each do |place, team|
-  team[:players].each do |player|
+  team.each do |attribute, data|
   binding.pry
 end
 end
